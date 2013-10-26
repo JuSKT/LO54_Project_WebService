761a5320f55aaf7b40b769ff90da51f1cab3be3e
Schnapse <Schnapse@SCHNAPSE-X131E>
2013-10-26 03:47:05 +0200

    Mise en place du web service CourseSession

--- LO54_Project/src/com/lo54project/webservice/service/CourseSessionResource.java
+++ LO54_Project/src/com/lo54project/webservice/service/CourseSessionResource.java
@@ -2,47 +2,92 @@ package com.lo54project.webservice.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import javax.xml.bind.JAXBElement;
 
 import com.lo54project.webservice.dao.CourseSessionDao;
 import com.lo54project.webservice.model.CourseSession;
 
+//Will map the resource to the URL course_sessions
 @Path("/coursesessions")
-public class CourseSessionResource {
-	 // Allows to insert contextual objects into the class, 
-	  // e.g. ServletContext, Request, Response, UriInfo
-	  @Context
-	  UriInfo uriInfo;
-	  @Context
-	  Request request;
+public class CourseSessionResource 
+{
+	// Allows to insert contextual objects into the class
+	@Context
+	UriInfo uriInfo;
+	@Context
+	Request request;
 	  
-	  // Return the list of course_sessions for applications
-	  @GET
-	  @Produces({MediaType.APPLICATION_JSON})
-	  public List<CourseSession> getCourseSessions() {
-	    List<CourseSession> courseSession = new ArrayList<CourseSession>();
+	// Return the list of course_sessions
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	public List<CourseSession> getCourseSessions() 
+	{
+		List<CourseSession> courseSession = new ArrayList<CourseSession>();
 	    courseSession.addAll(CourseSessionDao.instance.getModel().values());
 	    return courseSession; 
-	  }
+	}
 	  
-	  // Return the chosen course session
-	  @GET
-	  @Produces({MediaType.APPLICATION_JSON})
-	  @Path("{coursesession}")
-	  public CourseSession getCourseSession(@PathParam("coursesession") String id) {
-		  CourseSession cs = CourseSessionDao.instance.getModel().get(Integer.valueOf(id));
-		  if(cs==null){
-			  throw new RuntimeException("Get: Course session with " + id +  " not found");
-		  }
-		  return cs;
-	  }
-
+	// Return the chosen course_session
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	@Path("{coursesession}")
+	public CourseSession getCourseSession(@PathParam("coursesession") String id) 
+	{
+		CourseSession cs = CourseSessionDao.instance.getModel().get(Integer.valueOf(id));
+		
+		if(cs==null)
+		{
+			throw new RuntimeException("Get: Course session with " + id +  " not found");
+		}
+		
+		return cs;
+	}
+	
+	// Return the number of course_sessions
+	@GET
+	@Path("count")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getCount() 
+	{
+		int count = CourseSessionDao.instance.getModel().size();
+		return String.valueOf(count);
+	}
+	
+	// Put a course_session into response and return it 
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response putCourseSession(JAXBElement<CourseSession> course_session) 
+	{
+		CourseSession c = course_session.getValue();
+	    return putAndGetResponse(c);
+	}
+	  
+	// Put a course_session into response and return it
+	private Response putAndGetResponse(CourseSession c) 
+	{
+		Response res;
+		
+		if(CourseSessionDao.instance.getModel().containsKey(c.getId())) 
+		{
+			res = Response.noContent().build();
+	    } 
+		else 
+		{
+			res = Response.created(uriInfo.getAbsolutePath()).build();
+	    }
+		
+		CourseSessionDao.instance.getModel().put(c.getId(), c);
+	    return res;
+	}
 }
