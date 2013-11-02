ddfe57ab3f435388cbfacf5383f88defa5ab2d38
Schnapse <Schnapse@SCHNAPSE-X131E>
2013-11-02 22:39:49 +0100

    Instanciate differents handlers

--- LO54_Project/src/com/lo54project/webservice/servlet/IndexServlet.java
+++ LO54_Project/src/com/lo54project/webservice/servlet/IndexServlet.java
@@ -1,43 +1,67 @@
 package com.lo54project.webservice.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.lo54project.webservice.handler.CourseHandler;
+import com.lo54project.webservice.handler.CourseSessionHandler;
 import com.lo54project.webservice.handler.LocationHandler;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.UniformInterfaceException;
 
 /**
  * Servlet implementation class IndexServlet
  */
-//@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+public class IndexServlet extends HttpServlet 
+{
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public IndexServlet() 
+    {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		LocationHandler lh = new LocationHandler();
 		request.setAttribute("locations", lh.parseLocations());
+		
+		CourseSessionHandler csh = new CourseSessionHandler();
+		try {
+			request.setAttribute("coursesessions", csh.parseCourseSessions());
+		} catch (UniformInterfaceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClientHandlerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		CourseHandler ch = new CourseHandler();
+		request.setAttribute("courses", ch.parseCourses());
+		
 		getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		// TODO Auto-generated method stub
 	}
-
 }
