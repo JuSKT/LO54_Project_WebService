d09ecf83afe13cae700db4e8d6268201d391f34b
JuSKT <c_julien_perso@yahoo.fr>
2013-11-15 15:24:58 +0100

    Add interface for DAO

---
+++ LO54_Project/src/com/lo54project/webservice/dao/ClientDaoInterface.java
@@ -0,0 +1,9 @@
+package com.lo54project.webservice.dao;
+
+import java.util.Map;
+
+import com.lo54project.webservice.model.Client;
+
+public interface ClientDaoInterface {
+	public Map<Integer, Client> getModel();
+}
