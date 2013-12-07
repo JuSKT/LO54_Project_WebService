2153f7f68ae06950db6a8bb2f13036d1ce4fa529
ultrabook <ultrabook@ultrabook-PC.home>
2013-12-07 17:38:16 +0100

    test

--- LO54_Project/src/com/lo54project/webservice/dao/CourseSessionDao.java
+++ LO54_Project/src/com/lo54project/webservice/dao/CourseSessionDao.java
@@ -119,8 +119,7 @@ public enum CourseSessionDao implements DaoInterface {
 
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 	    SimpleDateFormat formatterBdd = new SimpleDateFormat("yyyy-MM-dd");
-        Date newDate = null;
-        
+        Date newDate = null;       
         try {
        	 
     	newDate = formatter.parse(date);  
